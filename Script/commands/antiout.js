6d2e2c4426266c7c40090f62da18af4ba331a8b0
shizuka101 <shizukaarav@gmail.com>
2026-04-12 11:30:27 +0600

    Update antiout.js

--- Script/commands/antiout.js
+++ Script/commands/antiout.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "antiout",
     version: "1.0.0",
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "—͟͞͞★ 𝗧𝗘𝗖 | 𝗧𝗛𝗥𝗢𝗡𝗘 𝗘𝗧𝗘𝗥𝗡𝗔𝗟 🌑 𝗖𝗟𝗔𝗡",
     hasPermssion: 1,
     description: "Turn off antiout",
     usages: "antiout on/off",
