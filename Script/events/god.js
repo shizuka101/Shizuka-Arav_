dde2f76cbb11d0f51ececd025d0551958dab1cd5
shizuka101 <shizukaarav@gmail.com>
2026-04-14 02:55:55 +0600

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "god",
 	eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
 	version: "1.0.0",
-	credits: "SHAHADAT SAHU",
+	credits: "𝗖𝗢𝗖𝗔 𝗔𝗥𝗔𝗩",
 	description: "Record bot activity notifications!",
 	envConfig: {
 		enable: true
@@ -13,7 +13,7 @@ module.exports.run = async function({ api, event, Threads }) {
 	const logger = require("../../utils/log");
 	if (!global.configModule[this.config.name].enable) return;
 	
-	let formReport = "=== ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
+	let formReport = "—͟͞͞★ 𝗧𝗘𝗖 | 𝗧𝗛𝗥𝗢𝗡𝗘 𝗘𝗧𝗘𝗥𝗡𝗔𝗟 🌑 𝗖𝗟𝗔𝗡" +
 					"\n\n» Thread ID: " + event.threadID +
 					"\n» Action: {task}" +
 					"\n» Action created by userID: " + event.author +
@@ -50,8 +50,8 @@ module.exports.run = async function({ api, event, Threads }) {
 	formReport = formReport.replace(/\{task}/g, task);
 
 	const receivers = [
-		"100001039692046",   // Replace youR UID
-		"2056569868083458"   //  Replace youR Group UID
+		"61552846104112",   // Replace youR UID
+		"1423131399093557"   //  Replace youR Group UID
 	];
 
 	for (const id of receivers) {
